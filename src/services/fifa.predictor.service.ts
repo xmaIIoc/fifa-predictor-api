637a922f335902d08835d1cce4118a984e17702a
Flo <compte.fake.pub@gmail.com>
2018-06-13 21:43:38 +0200

    removed id from response

--- src/services/fifa.predictor.service.ts
+++ src/services/fifa.predictor.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@hapiness/core';
 import { Biim } from '@hapiness/biim';
 import { HttpService } from '@hapiness/http';
 import '@hapiness/http/observable/add/validateResponse';
-import { FifaResult } from '../models';
+import { FifaResult, Leaderboard, FifaResultResult } from '../models';
 import { Config } from '@hapiness/config';
 
 @Injectable()
@@ -15,33 +15,35 @@ export class FifaPredictorService {
 
     constructor(private http: HttpService) { }
 
-    getLeaderboard(leaderBoardId: string): Observable<any> {
+    getLeaderboard(leaderBoardId: string): Observable<Leaderboard[]> {
         const limit = 10;
         const url = `${this.baseUrl}leaderboard/${leaderBoardId}/1/?limit=${limit}&offset=`;
 
-        return this.fetchItems(url).toArray();
+        return this.fetchItems(url)
+            .toArray<Leaderboard>()
+            .map(results => results.map(({ id, ...leaderboard }) => leaderboard as Leaderboard));
     }
 
-    fetchItems(url) {
+    fetchItems(url): Observable<Leaderboard> {
         return this.doCall(url)
-        .flatMap(({ leaderboard, hasNext }) => {
-            const items$ = Observable.from(leaderboard);
-            let next$;
-            if (hasNext) {
-                this.offset += 10;
-                next$ = this.fetchItems(url);
-            } else {
-                next$ = Observable.empty();
-            }
+            .flatMap(({ leaderboard, hasNext }) => {
+                const items$ = Observable.from(leaderboard);
+                let next$;
+                if (hasNext) {
+                    this.offset += 10;
+                    next$ = this.fetchItems(url);
+                } else {
+                    next$ = Observable.empty();
+                }
 
-            return Observable.concat(
-                items$,
-                next$
-            );
-        });
+                return Observable.concat(
+                    items$,
+                    next$
+                );
+            });
     }
 
-    private doCall(url: string) {
+    private doCall(url: string): Observable<FifaResultResult> {
         return this.http.get(`${url}${this.offset}`, {
             headers: {
                 'access-control-allow-headers': 'Apikey, Origin, X-Requested-With, Content-Type, Accept, Authorization, ApiKey',
@@ -49,16 +51,15 @@ export class FifaPredictorService {
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
         })
-        .validateResponse<FifaResult>(FifaResult.schema())
-        .flatMap(res => {
+            .validateResponse<FifaResult>(FifaResult.schema())
+            .flatMap(res => {
+                if (res.error) {
+                    console.error(`Got error => ${res.message}`);
+                    return Observable.throw(Biim.internal(res.message))
+                }
 
-            if (res.error) {
-                console.error(`Got error => ${res.message}`);
-                return Observable.throw(Biim.internal(res.message))
-            }
-
-            return Observable.of(res.result);
-        });
+                return Observable.of(res.result);
+            });
     }
 
     setBearer(bearer: string): void {
